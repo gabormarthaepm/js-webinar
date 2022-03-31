c9851f3d75cb7b8b31e2e412f241559c7936490b
Gabor.Martha <gabor_martha@epam.com>
2022-03-31 10:05:26 +0200

    adding 1st module tasks

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -18,4 +18,70 @@ describe.only('calc', () => {
      */
     // TODO: write test cases to test calculator
 
+    it("should have proper value", () => {
+        const c = calc(3).v;
+        expect(c).not.to.be.undefined;
+    });
+
+    describe.only("add", () => {
+
+        it("should exist", () => {
+            // Given
+            // When
+            // Then
+            
+            calc(3).add(5).v
+        });
+
+        it("should be able to add a number to the current value", () => {
+            const c = calc(3);
+            const result = c.add(5).v;
+            expect(result).to.equal(8);
+        });
+
+    })
+
+    describe.only("minus", () => {
+
+        it("should exist", () => {
+            calc(3).minus(2).v;
+        });
+
+        it("should be able to substract a number from the current value", () => {
+            const c = calc(3);
+            const result = c.minus(2).v;
+            expect(result).to.equal(1);
+        });
+
+    })
+
+    describe.only("divide", () => {
+
+        it("should exist", () => {
+            // Given
+            const c = calc(42);
+            // When
+            // Then
+            expect(c.add).not.to.be.undefined;
+        });
+
+        it("should be able to perform a valid division", () => {
+            //Given
+            const c = calc(10);
+            //When
+            const result = c.divide(2).v;
+            //Then
+            expect(result).to.equal(5);
+        });
+
+        it("should handle the division by zero", () => {
+            //Given
+            const c = calc(42);
+            //When
+            //Then
+            expect(() => c.divide,bind(null, 0)).to.throw("Division");
+            expect(c.divide,bind(null, 0)).to.throw();
+        })
+
+    })
 });
