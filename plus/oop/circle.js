105725eacadca68f6f5d7c40dd9e19f9333ecff9
gabormartha <robag.ahtram@gmail.com>
2022-04-13 10:27:03 +0200

    circle.js  created

--- plus/oop/circle.js
+++ plus/oop/circle.js
@@ -6,3 +6,25 @@
  * @function area return the area of the circle
  * @function perimeter return the perimeter of the circle
  */
+
+'use strict'
+
+class Circle {
+    constructor(r){
+        this.r = r;
+
+        if (r < 0) {
+            throw Error;
+        }
+    }
+
+    getArea(){
+        return Math.pow(this.r, 2) * Math.PI;
+    }
+
+    getPerimeter() {
+        return 2 * (Math.PI * this.r);
+    }
+}
+
+module.exports = Circle;
