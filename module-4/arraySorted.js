db29903f58afd94bb917b9cb9667717082378343
gabormartha <robag.ahtram@gmail.com>
2022-05-10 08:24:18 +0200

    arraySorted.js created

--- module-4/arraySorted.js
+++ module-4/arraySorted.js
@@ -12,3 +12,35 @@
  * @returns {boolean} true if the array is properly sorted,
  *                    false otherwise
  */
+
+ function arraySorted(items, ignore) {
+
+    let itemsArrayCorrected = items.map(item =>item.toString().toLowerCase().replace(/\s/g, ""));
+
+    function removeAll(s, c) {
+        while (s.includes(c)) {
+            s = s.replace(c, "");
+        }
+        return s;
+    }
+
+    if (ignore) {
+        let ignoredCharsArray = ignore.split("");
+        ignoredCharsArray.forEach(onechar => 
+            itemsArrayCorrected = itemsArrayCorrected.map(correctedItem => {
+                return removeAll(correctedItem, onechar);
+                }
+            )
+            );
+    }
+
+    let itemsArraySorted = [...itemsArrayCorrected].sort();
+
+    for (let i = 0; i < itemsArraySorted.length; i++) {
+        if (itemsArraySorted[i] != itemsArrayCorrected[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+module.exports = arraySorted;
